c2c98c1b9e95584ef6e1d33126f6c4998adeced9
Bryce Billingsley <babster24@gmail.com>
2022-06-29 16:54:53 -0500

    finished

--- index.js
+++ index.js
@@ -20,16 +20,25 @@ function addMovie(event) {
 
 function deleteMovie(event) {
   event.target.parentNode.remove();
-  message.textContent = "Movie Deleted";
+  message.textContent = `${event.target.previousSibling.textContent} Deleted`;
+  revealMessage();
 }
 
 function crossOffMovie(event) {
   event.target.classList.toggle("checked");
   if (event.target.classList.contains("checked")) {
-    message.textContent = "Movie Watched";
+    message.textContent = `${event.target.textContent} Watched`;
   } else {
-    message.textContent = "Movie added back";
+    message.textContent = `${event.target.textContent} added back`;
   }
+  revealMessage();
+}
+
+function revealMessage() {
+  message.classList.remove("hide");
+  setTimeout(() => {
+    message.classList.add("hide");
+  }, 1000);
 }
 
 const form = document.querySelector("form");
